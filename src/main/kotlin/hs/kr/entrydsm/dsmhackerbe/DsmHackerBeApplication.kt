91fb7c943553b022f00f7eae49e43f93228bcd05
박주원 <qkrwndnjs7308@gmail.com>
2025-07-16 18:28:58 +0900

    DD

--- src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/DsmHackerBeApplication.kt
+++ src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/DsmHackerBeApplication.kt
@@ -4,9 +4,17 @@ import hs.kr.entrydsm.dsmhackerbe.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties::class)
+@EnableJpaRepositories(basePackages = ["hs.kr.entrydsm.dsmhackerbe.domain.*.repository"], 
+                       excludeFilters = [org.springframework.context.annotation.ComponentScan.Filter(
+                           type = org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE,
+                           classes = [hs.kr.entrydsm.dsmhackerbe.domain.auth.repository.RefreshTokenRepository::class]
+                       )])
+@EnableRedisRepositories(basePackages = ["hs.kr.entrydsm.dsmhackerbe.domain.auth.repository"])
 class DsmHackerBeApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/domain/auth/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/domain/auth/repository/RefreshTokenRepository.kt
@@ -2,7 +2,9 @@ package hs.kr.entrydsm.dsmhackerbe.domain.auth.repository
 
 import hs.kr.entrydsm.dsmhackerbe.domain.auth.entity.RefreshToken
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
     fun findByToken(token: String): RefreshToken?
 }
