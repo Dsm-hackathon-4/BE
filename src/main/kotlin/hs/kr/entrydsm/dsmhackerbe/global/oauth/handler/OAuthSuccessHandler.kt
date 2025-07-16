35acdd5be643210c09caa11baa21d80bb4e3ffeb
박주원 <qkrwndnjs7308@gmail.com>
2025-07-16 23:41:07 +0900

    ㅇㅇㅇㅇ
    ㅇㅇㅇㅇ

--- src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/global/oauth/handler/OAuthSuccessHandler.kt
+++ src/main/kotlin/hs/kr/entrydsm/dsmhackerbe/global/oauth/handler/OAuthSuccessHandler.kt
@@ -26,7 +26,7 @@ class OAuthSuccessHandler(
         )
 
         val redirectUrl = UriComponentsBuilder
-            .fromUriString("https://hacker2.parkjoowon.site/oauth/success")
+            .fromUriString("localhost:5173/oauth/success")
             .queryParam("accessToken", tokenResponse.accessToken)
             .queryParam("refreshToken", tokenResponse.refreshToken)
             .build().toUriString()
